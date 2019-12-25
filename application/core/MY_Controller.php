1f9baa8cffb23cc7083a44dc13e36c7642446781
Elxan123 <elxan.aslanov2000@gmail.com>
2019-12-25 23:44:20 +0400

    events

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -8,9 +8,9 @@ class MY_Controller extends CI_Controller{
     {
         parent::__construct();
 
-//        if (empty($this->session->userdata("user"))){
-//                redirect(base_url("Admin"));
-//        }
+        if (empty($this->session->userdata("user"))){
+                redirect(base_url("Admin"));
+        }
 
     }
 
